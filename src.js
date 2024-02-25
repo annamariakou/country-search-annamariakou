60b4279ef34afbbf87e1f043f65df76675eec464
annamariakoutsoras <annamariakoutsoras@Annamarias-MacBook-Air>
2024-02-25 11:11:44 +0000

    pull data from api

--- src.js
+++ src.js
@@ -5,3 +5,34 @@ document.getElementById("searchBar").addEventListener('keypress', function (e) {
     }
 });
 });
+
+const url = "https://restcountries.com/v3.1/all";
+
+
+async function fetchAllCountryData() {
+    // Declare a variabble to store the HTTP response 
+    const response = await fetch (url, {
+    headers: {
+    Accept: "application/json",
+    },
+    });
+    
+    // if the request fails log out an error 
+    
+    if (!response.ok) {
+    console.error(response.status);
+    console.error(response.text());
+    return "Sorry the no countries today!😭";
+    }
+    
+    // parse the response body or data as JSON 
+    
+    const data = await response.json();
+    
+    // Log out the joke data 
+    
+    console.log(data); 
+    
+    }
+
+    
